23a96f35f4e3e9752987fdbf046301ad871040a5
Kshitij <kd57237@gmail.com>
2019-04-04 20:50:46 +0200

    added demo

---
+++ RSA_analysis/demo.m
@@ -0,0 +1,9 @@
+clear;clc; close all hidden;
+
+% directory containing saved taskonomy rdms
+rdm_dir = '/media/kshitid20/My Passport1/Taskonomy_data/paper_data/RDM_taskonomy_on_taskonomy_500';
+% directory of downloaded rsatoolbox
+rsa_dir = '/media/kshitid20/My Passport1/Taskonomy_data/rsatoolbox';
+% directory to save the results
+save_dir = './Fig5';
+C_Fig5(rdm_dir,save_dir,rsa_dir)
